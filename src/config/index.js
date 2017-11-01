f0ac1ed1bdbffe9df428a82bf38aa15d122ff9fc
Jon <largo.npc@gmail.com>
2017-11-01 14:29:23 +0000

    Master (#12)
    * Added MongoDB and removed AWS DynamoDB (#9)
    
    * Destiny 2 trial
    
    * It's a promise!
    
    * Stupid lock files
    
    * Pass name
    
    * Moved from AWS DynamoDB to MongoDB (#8)
    
    * Removed destiny from develop
    
    * Added MongoDB
    
    * Changed MONGODB env var name
    
    * Insert tests
    
    * Fixed dupe decl
    
    * Testing insert
    
    * Updated dc
    
    * Cheeky!
    
    * Removed test from app start
    
    * Get record added
    
    * Log data out
    
    * Added await
    
    * Added awaits
    
    * Promises
    
    * Why undefined?
    
    * Promises suck sometimes
    
    * update records
    
    * Updated location for test update record
    
    * Upsert
    
    * Fixed incorrect var usage
    
    * Completed port to mongo?
    
    * Fixed bug where doesn't update config
    
    * Swapped err and data around. What happened to consistancy?
    
    * Update from dev (#10)
    
    * Destiny 2 trial
    
    * It's a promise!
    
    * Stupid lock files
    
    * Pass name
    
    * Removed destiny from develop
    
    * Moved from AWS DynamoDB to MongoDB (#8)
    
    * Removed destiny from develop
    
    * Added MongoDB
    
    * Changed MONGODB env var name
    
    * Insert tests
    
    * Fixed dupe decl
    
    * Testing insert
    
    * Updated dc
    
    * Cheeky!
    
    * Removed test from app start
    
    * Get record added
    
    * Log data out
    
    * Added await
    
    * Added awaits
    
    * Promises
    
    * Why undefined?
    
    * Promises suck sometimes
    
    * update records
    
    * Updated location for test update record
    
    * Upsert
    
    * Fixed incorrect var usage
    
    * Completed port to mongo?
    
    * Fixed bug where doesn't update config
    
    * Swapped err and data around. What happened to consistancy?
    
    * Added save to db is not present

--- src/config/index.js
+++ src/config/index.js
@@ -1,112 +1,34 @@
 import defaultConfig from './config.default.json';
-import AWS from 'aws-sdk';
+import { getRecord, updateRecord } from '../database';
 
 let config = defaultConfig;
-let redis;
 
-const baseAWSParams = {
-    TableName: process.env.AWS_TABLE_NAME
-};
-
-const ddb = new AWS.DynamoDB({
-    region: 'eu-west-1'
-});
-
-const UpdateAWSConfig = async () => {
-    const promise = ddb.updateItem(
-        Object.assign(
-            {},
-            baseAWSParams,
-            {
-                Key: {
-                    'id': {
-                        S: '1'
-                    }
-                },
-                UpdateExpression: "set config = :c",
-                ExpressionAttributeValues:{
-                    ":c": JSON.stringify(config)
-                },
-                ReturnValues:"UPDATED_NEW"
-            }
-        )
-    ).promise();
-    
-    return await promise.then((data, err) => {
+const SaveConfig = async () => {
+    return new Promise((resolve, reject) => {
+        updateRecord('config', {}, config).then((data, err) => {
             if (err) {
-                console.log("Error updating config in DB", err);
-            } else {
-               console.log("Config updated in AWS");
+                console.log("Error writing item", err);
+                return;
             }
-        }
-    );
-};
 
-const GetAWSConfig = async () => {
-    const getPromise = ddb.getItem(
-        Object.assign(
-            {},
-            baseAWSParams,
-            {
-                Key: {
-                    'id': {
-                        S: '1'
-                    }
-                }
-            }
-        )
-    ).promise();
-
-    return await getPromise.then((data, err) => {
-        if (!err && data.Item && data.Item.config) {
-            return JSON.parse(data.Item.config.S);
-        }
-
-        if (err) {
-            console.log("Error", err);
-        }
-
-        SaveAWSConfig();
-        return config;
-    });
-};
-
-const SaveAWSConfig = async () => {
-    const promise = ddb.putItem(
-        Object.assign(
-            {},
-            baseAWSParams,
-            {
-                Item: {
-                    'id': {
-                        S: '1'
-                    },
-                    'config': {
-                        S: JSON.stringify(config)
-                    }
-                }
-            }
-        )
-    ).promise();
-
-    return await promise.then((data, err) => {
-        if (err) {
-            console.log("Error writing new item", err);
-            return;
-        } else {
             console.log("Written config to database", data);
             return data;
-        }
+        });
     });
 };
 
 export const LoadConfig = async () => {
-    await GetAWSConfig().then((data) => {
-        if (data) {
-            config = data;
-        }
-    });
-    console.log('Config fetched from AWS: ', config);
+    await getRecord('config')
+        .then((data) => {
+            if (data) {
+                config = data;
+                console.log('Config fetched from MongoDB: ', config);
+            } else {
+                console.log('Loaded default config: ', config);
+                SaveConfig();
+                console.log('Storing config to database');
+            }
+        });
 
     return config;
 };
@@ -139,5 +61,5 @@ export const UpdateConfig = (prop, value) => {
     }
 
     config[prop] = ConvertToType(value);
-    SaveAWSConfig(config);
+    SaveConfig();
 };
