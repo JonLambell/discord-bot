dc30a3304b7ec7dc53e77021bcf84cb277f900a9
Jon Lambell <jonathan.lambell@capitalone.com>
2017-11-01 07:52:21 +0000

    Changed MONGODB env var name

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 export const connectTest = () => {
-    MongoClient.connect(process.env.MONGO_URL, (err, db) => {
+    MongoClient.connect(process.env.MONGODB_URI, (err, db) => {
         if (err) {
             console.log(err);
         }
